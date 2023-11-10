70fb3d05f60aff2fcbffb5c929d0a216ffd113a0
Joas-Assuncao <joassalvior@gmail.com>
2023-11-10 01:12:17 -0300

    fix: improve table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,28 +1,11 @@
-import { useMemo } from "react";
-
-import { Tbody } from "./components/Tbody";
-import { Thead } from "./components/Thead";
-
-export function Table({ dataSource }: { dataSource: object[] }) {
-  const columnsHeader = useMemo(
-    () => (dataSource[0] ? Object.keys(dataSource[0]) : []),
-    [dataSource]
-  );
-  const columnsBody: [string, string][][] = useMemo(
-    () => dataSource.map((data) => Object.entries(data)),
-    [dataSource]
-  );
-
-  if (dataSource.length === 0) return <></>;
-
+export function TableContainer({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
           <div className="overflow-hidden">
             <table className="min-w-full text-left text-sm font-light">
-              <Thead rowHeader={columnsHeader} />
-              <Tbody rowBody={columnsBody} />
+              {children}
             </table>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,23 @@ import Head from "next/head";
 
 import { ErrorFetching } from "@/components/ErrorFetching";
 import { useDrivers } from "@/hooks/useDrivers";
-import { Table } from "@/components/Table";
+import { TableContainer } from "@/components/Table";
+import { Thead } from "@/components/Table/components/Thead";
+import { Tbody } from "@/components/Table/components/Tbody";
+import { useMemo } from "react";
+import { IDriver } from "@/models/Driver";
 
 export default function Home() {
   const { drivers, error, isLoading } = useDrivers();
+
+  const columnsHeader = useMemo(
+    () => (drivers[0] ? Object.keys(drivers[0]) : []),
+    [drivers]
+  );
+  const columnsBody: [string, string][][] = useMemo(
+    () => drivers.map((driver: IDriver) => Object.entries(driver)),
+    [drivers]
+  );
 
   return (
     <>
@@ -14,8 +27,14 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl mb-4">Motoristas</h1>
-        <ErrorFetching hasError={error || !drivers[0]} />
-        <Table dataSource={drivers} />
+        <ErrorFetching
+          hasError={error || !drivers[0]}
+          message="Nenhum motorista registrado"
+        />
+        <TableContainer>
+          <Thead rowHeader={columnsHeader} />
+          <Tbody rowBody={columnsBody} />
+        </TableContainer>
       </main>
     </>
   );
